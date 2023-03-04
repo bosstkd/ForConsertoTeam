37202f458fae5be55d6052de656c90f871c38298
Mohammed El Amine <a-ek@hotmail.fr>
2023-03-04 01:33:04 +0100

    Ajout OCR

---
+++ src/main/java/com/jmag/projet/application/OcrService.java
@@ -0,0 +1,60 @@
+package com.jmag.projet.application;
+
+import lombok.RequiredArgsConstructor;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+
+@RequiredArgsConstructor
+@Service
+public class OcrService {
+
+    public static final int DEFAULT_BUFFER_SIZE = 8192;
+    private static String FILE_SOURCE_PATH = "tessFiles/markFile.tkt";
+
+    public String getStringFromFile(InputStream inputStream)
+            throws TesseractException, IOException, URISyntaxException {
+
+
+        File file = new File(getTessDataPath("tessFiles") + "/" + buildFileName("jpg"));
+        copyInputStreamToFile(inputStream, file);
+        Tesseract tesseract = new Tesseract();
+        tesseract.setDatapath(getTessDataPath("tessdata"));
+        tesseract.setLanguage("fra+eng+ara");
+        tesseract.setPageSegMode(1);
+        tesseract.setOcrEngineMode(1);
+        var result = tesseract.doOCR(file);
+        file.delete();
+        return result;
+    }
+
+    private String getTessDataPath(String folderName) throws URISyntaxException {
+        String tessdataPath = Paths.get(ClassLoader.getSystemResource(FILE_SOURCE_PATH).toURI()).toUri().getPath();
+        return tessdataPath.substring(1, tessdataPath.indexOf("target")) + folderName;
+    }
+
+    private void copyInputStreamToFile(InputStream inputStream, File file)
+            throws IOException {
+
+        // append = false
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            int read;
+            byte[] bytes = new byte[DEFAULT_BUFFER_SIZE];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+        }
+    }
+
+    private String buildFileName(final String extension) {
+
+        return System.currentTimeMillis() + "." + extension;
+    }
+}
--- src/main/java/com/jmag/projet/presentation/TestController.java
+++ src/main/java/com/jmag/projet/presentation/TestController.java
@@ -1,10 +1,17 @@
 package com.jmag.projet.presentation;
 
 import com.jmag.projet.application.NumbersService;
+import com.jmag.projet.application.OcrService;
 import com.jmag.projet.application.PlanerTreeService;
 import com.jmag.projet.domain.test.PlanerTree;
 import lombok.RequiredArgsConstructor;
+import net.sourceforge.tess4j.TesseractException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 @RestController("/")
 @CrossOrigin("*")
@@ -14,6 +21,8 @@ public class TestController {
     private final PlanerTreeService planerTreeService;
     private final NumbersService numbersService;
 
+    private final OcrService ocrService;
+
     @PostMapping("/addPlanerTree")
     public PlanerTree addPlanerTree(@RequestBody PlanerTree child) {
         return planerTreeService.addChild(child);
@@ -22,5 +31,12 @@ public class TestController {
     @GetMapping("/getRomanNumber")
     public String getRomanNumber(@RequestParam("number") String number) {
         return numbersService.getRomanFromArabicNumber(number);
+    }
+
+    @PostMapping("/getString")
+    public String getString(@RequestParam("file") MultipartFile file) throws IOException, TesseractException, URISyntaxException {
+        var inputStream = file.getInputStream();
+
+        return ocrService.getStringFromFile(inputStream);
     }
 }
